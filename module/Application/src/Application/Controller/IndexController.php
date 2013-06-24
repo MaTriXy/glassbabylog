5936c4a60ac1fdafc170e8f0ba5212273b9bf6d3
Alex Dennis <alex.dennis@gmail.com>
2013-06-23 23:49:20 -0400

    Getting G+ access token successfully

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -25,10 +25,8 @@ class IndexController extends AbstractActionController
         $this->config = $config;
     }
 
-    public function indexAction()
+    protected function getGoogleClient()
     {
-        session_start();
-
         $client = new \Google_Client();
         $client->setApplicationName("glassbabylog");
 
@@ -38,113 +36,104 @@ class IndexController extends AbstractActionController
         $client->setClientSecret($this->config['client_secret']);
         $client->setRedirectUri($this->config['redirect_uri']);
         $client->setDeveloperKey($this->config['developer_key']);
-        $plus = new \Google_PlusService($client);
+        return $client;
+    }
 
-        if (isset($_GET['logout'])) {
-          unset($_SESSION['token']);
-        }
+    public function indexAction()
+    {
+        session_start();
 
-        if (isset($_GET['code'])) {
-          if (strval($_SESSION['state']) !== strval($_GET['state'])) {
-            die("The session state did not match.");
-          }
-          $client->authenticate();
-          $_SESSION['token'] = $client->getAccessToken();
-          $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-          header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
-        }
-
-        if (isset($_SESSION['token'])) {
-          $client->setAccessToken($_SESSION['token']);
-        }
-
-        if ($client->getAccessToken()) {
-          $me = $plus->people->get('me');
-          print "Your Profile: <pre>" . print_r($me, true) . "</pre>";
-
-          $params = array('maxResults' => 100);
-          $activities = $plus->activities->listActivities('me', 'public', $params);
-          print "Your Activities: <pre>" . print_r($activities, true) . "</pre>";
-          
-          $params = array(
-            'orderBy' => 'best',
-            'maxResults' => '20',
-          );
-          $results = $plus->activities->search('Google+ API', $params);
-          foreach($results['items'] as $result) {
-            print "Search Result: <pre>{$result['object']['content']}</pre>\n";
-          }
-
-          // The access token may have been updated lazily.
-          $_SESSION['token'] = $client->getAccessToken();
-        } else {
-          $state = mt_rand();
-          $client->setState($state);
-          $_SESSION['state'] = $state;
-
-          $authUrl = $client->createAuthUrl();
-          print "<a class='login' href='$authUrl'>Connect Me!</a>";
-        }
+        $state = md5(rand());
+        $_SESSION['state'] = $state;
 
         return new ViewModel(array(
-          'CLIENT_ID' => $this->config['client_id']
+          'CLIENT_ID' => $this->config['client_id'],
+          'STATE'     => $state
         ));
     }
 
-    public function storeTokenAction()
+
+    // Upgrade given auth code to token, and store it in the session.
+    // POST body of request should be the authorization code.
+    // Example URI: /connect?state=...&gplus_id=...
+    public function connectAction() 
     {
-      session_start();
-      $client = new \Google_Client();
-      $client->setApplicationName("glassbabylog");
+        session_start();
 
-      // Visit https://code.google.com/apis/console?api=plus to generate your
-      // client id, client secret, and to register your redirect uri.
-      $client->setClientId($this->config['client_id']);
-      $client->setClientSecret($this->config['client_secret']);
-      $client->setRedirectUri($this->config['redirect_uri']);
-      $client->setDeveloperKey($this->config['developer_key']);
-      $plus = new \Google_PlusService($client);
+        $token = $_SESSION['token'];
+        if(empty($token)) {
 
-      $request = $this->getRequest();
-      $code = $request->getContent();
-      // $gPlusId = $request->get['gplus_id'];
-      // Exchange the OAuth 2.0 authorization code for user credentials.
-      $client->authenticate($code);
+          // Ensure that this is no request forgery going on, and that the user
+          // sending us this connect request is the user that was supposed to.
+          if ($this->getRequest()->getQuery('state') != $_SESSION['state']) {
+            $this->getResponse()->setStatusCode(401);   
+            $this->getResponse()->setContent('Invalid state parameter');
+            return $this->getResponse();
+          }
 
-      $token = json_decode($client->getAccessToken());
-      var_dump($token);
-      exit;
-      // Verify the token
-      $reqUrl = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=' .
-              $token->access_token;
-      $req = new Google_HttpRequest($reqUrl);
+          $client = $this->getGoogleClient();
 
-      $tokenInfo = json_decode(
-          $client::getIo()->authenticatedRequest($req)->getResponseBody());
+          // Normally the state would be a one-time use token, however in our
+          // simple case, we want a user to be able to connect and disconnect
+          // without reloading the page.  Thus, for demonstration, we don't
+          // implement this best practice.
+          //$app['session']->set('state', '');
 
-      // If there was an error in the token info, abort.
-      if ($tokenInfo->error) {
-        $this->getResponse()->setStatusCode(500);
-        $this->getResponse()->setContent($tokenInfo->error);
-        return;
-        // return new Response($tokenInfo->error, 500);
-      }
-      // Make sure the token we got is for the intended user.
-      // if ($tokenInfo->userid != $gPlusId) {
-      //   return new Response(
-      //       "Token's user ID doesn't match given user ID", 401);
-      // }
-      // Make sure the token we got is for our app.
-      if ($tokenInfo->audience != $this->config['client_id']) {
-        $this->getResponse()->setStatusCode(401);
-        $this->getResponse()->setContent("Token's client ID does not match app's.");
-        return;
-      }
+          $code = $this->getRequest()->getContent();
+          // Exchange the OAuth 2.0 authorization code for user credentials.
+          $client->authenticate($code);
+          $token = json_decode($client->getAccessToken());
 
-      // Store the token in the session for later use.
-      // $app['session']->set('token', json_encode($token));
-      $_SESSION['token'] = json_encode($token);
-      $this->getResponse()->setContent('Succesfully connected with token: ' . print_r($token, true));
+          // You can read the Google user ID in the ID token.
+          // "sub" represents the ID token subscriber which in our case 
+          // is the user ID. This sample does not use the user ID.
+          $attributes = $client->verifyIdToken($token->id_token, $this->config['client_id'])
+              ->getAttributes();
+          $gplus_id = $attributes["payload"]["sub"];
+
+          // Store the token in the session for later use.
+          $_SESSION['token'] = json_encode($token);
+          $response = 'Successfully connected with token: ' . print_r($token, true);
+        }
+
+        $this->getResponse()->setContent($respone);
+        return $this->getResponse();
+    }
+
+    // Get list of people user has shared with this app.
+    public function peopleAction()
+    {
+        session_start();
+
+        $client = $this->getGoogleClient();
+
+        $plus = new \Google_PlusService($client);
+
+        $token = $_SESSION['token'];
+        if (empty($token)) {
+            $this->getResponse()->setStatusCode(401);
+            return $this->getResponse();
+          // return new Response('Unauthorized request', 401);
+        }
+        $client->setAccessToken($token);
+        $people = $plus->people->listPeople('me', 'visible', array());
+        return new JsonModel($people);
+    }
+
+
+    public function disconnectAction()
+    {
+        session_start();
+
+        $client = $this->getGoogleClient();
+
+        $token = json_decode($_SESSION['token'])->access_token;
+        $client->revokeToken($token);
+
+        // Remove the credentials from the user's session.
+        $_SESSION['token'] = '';
+        $this->getResponse()->setContent('Successfully disconnected');
+        return $this->getResponse();
     }
 
     public function listAction()
